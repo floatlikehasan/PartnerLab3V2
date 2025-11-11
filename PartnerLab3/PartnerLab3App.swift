a3f8b674bfb9434750b68e7520347984bb777f3f
Hasan Dababo <hasand34500@gmail.com>
2025-11-10 23:27:44 -0800

    Initial Commit

---
+++ PartnerLab3/PartnerLab3App.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerLab3App.swift
+//  PartnerLab3
+//
+//  Created by cisstudent on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PartnerLab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
